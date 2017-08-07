45bd02e014f80ee64ccf30b66dc4a4f59c848792
zamhaq <haque864@gmail.com>
2017-08-07 15:05:01 +0530

    Added URL validation and proper status code

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const seedrandom = require('seedrandom');
+const validUrl = require('valid-url');
+
 const {mongoose} = require('./db/mongoose');
 const {url} = require('./model/url');
 
@@ -10,29 +12,36 @@ app.get('/',(req,res) => {
 
 app.get('/new/*',(req,res) => {
   const requrl = req.params[0];
-  let hostname = req.hostname;
-  Math.seedrandom(requrl);
-  const now = Date.now();
-  const rnum = Math.floor(Math.random() * (now/10000000));
-  hostname = hostname + ':3000/' + rnum;
-    var newurl = new url({
-        url : requrl,
-        rnum : rnum,
-        hostname : hostname
-  });
-    newurl.save().then((savedurl) => {
-        res.send(savedurl);
-    }).catch((e) => res.send(e));
+  if(validUrl.isHttpUri(requrl) || validUrl.isHttpsUri(requrl))
+  {
+      let hostname = req.hostname;
+      Math.seedrandom(requrl);
+      const now = Date.now();
+      const rnum = Math.floor(Math.random() * (now/10000000));
+      hostname = hostname + ':3000/' + rnum;
+      var newurl = new url({
+            url : requrl,
+            rnum : rnum,
+            hostname : hostname
+      });
+      newurl.save().then((savedurl) => {
+            res.send(savedurl);
+        }).catch((e) => res.send(e));
+    }
+    else{
+      res.status(400).send('Invalid URL');
+    }
 });
 
 app.get('/:shorturl',(req,res) => {
   if(!isNaN(req.params.shorturl)){
-    let hostname = req.hostname;
-      console.log("This is a number");
-      url.findOne({hostname})
+    let rnum = Number(req.params.shorturl);
+      url.findOne({rnum}).then((url) => {
+        res.redirect(url.url);
+      }).catch((e) => res.status(404).send(e));
     }
-      else{
-        res.send("Not a number");
+    else{
+        res.status(400).send("Not a valid shortened url");
     }
 });
 
