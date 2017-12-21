889c2ea1e1b53db3deee6770099a25d2519e1881
Rishi Raj <rishiraj6686@gmail.com>
2017-12-21 23:27:50 +0530

    Removed logging and placeholder value

--- server/public/script.js
+++ server/public/script.js
@@ -1,8 +1,6 @@
 $('#input_form').submit(function() {
     const val = $('#input_link').val();
-    console.log('val',val);
     $.get('/new/'+val,function(data) {
-        console.log('data',data);
         $('#output_link').val(data.short_url);
         $('#input_div > h2').val("Try for another URL")
         $('#output').show();
